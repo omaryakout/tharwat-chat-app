86e723e1a83162211cb7b686b7f4aa6fd76c3fa3
omar yakout <omaryakout44@gmail.com>
2024-09-02 15:36:11 +0300

    second

--- lib/pages/login_cubit/login_cubit.dart
+++ lib/pages/login_cubit/login_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
@@ -7,14 +9,23 @@ part 'login_state.dart';
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
   @override
-  Future<void> loginUser({required String email , required String password}) async {
+  Future<void> loginUser(
+      {required String email, required String password}) async {
     emit(LoginLoading());
     try {
       UserCredential user = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
       emit(LoginSucess());
-    } on Exception catch (e) {
-      emit(LoginFailure());
+    } on FirebaseAuthException catch (ex) {
+      if (ex.code == 'user-not-found') {
+        emit(LoginFailure(errMessage: 'user not found'));
+      } else if (ex.code == 'wrong-password') {
+        emit(LoginFailure(errMessage: 'wrong password'));
+      } else if (ex.code == 'email-not-found') {
+        emit(LoginFailure(errMessage: 'The email address is badly formatted'));
+      }
+    } catch (e) {
+      emit(LoginFailure(errMessage: 'something went wrong'));
     }
   }
 }
--- lib/pages/login_cubit/login_state.dart
+++ lib/pages/login_cubit/login_state.dart
@@ -3,7 +3,13 @@ part of 'login_cubit.dart';
 @immutable
 abstract class LoginState {}
 
- class LoginInitial extends LoginState {}
- class LoginLoading extends LoginState {}
- class LoginSucess extends LoginState {}
- class LoginFailure extends LoginState {}
+class LoginInitial extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginSucess extends LoginState {}
+
+class LoginFailure extends LoginState {
+  String errMessage;
+  LoginFailure({required this.errMessage});
+}
